2d2e5ebc9c18d087dd424974caf8a91fbb99f5d9
Chantal St Louis <chantalstlouis@akauk.com>
2020-06-10 15:19:18 +0100

    Updates code to use single quotes instead of doubles

--- src/components/our-services/our-services.component.jsx
+++ src/components/our-services/our-services.component.jsx
@@ -8,63 +8,63 @@ class OurServices extends Component {
 
     render() {
         return (
-            <section id="our-services" class="our-services">
+            <section id='our-services' class='our-services'>
                 <div className='container'>
-                    <div class="content-wrapper">
-                        <h1 data-aos="fade-right">
+                    <div class='content-wrapper'>
+                        <h1 data-aos='fade-right'>
                             Our
           <span>services.</span>
                         </h1>
 
-                        <div class="flex-container">
-                            <div class="services">
-                                <div class="services__group services__group--1">
-                                    <div class="content-wrapper" v-for="(obj, key) in services1" :key="key">
-                <div class="service">
-                                        <div class="img-wrapper">
-                                            <img :src="obj.serviceIcn" :alt="obj.alt" data-aos="fade-left" />
+                        <div class='flex-container'>
+                            <div class='services'>
+                                <div class='services__group services__group--1'>
+                                    <div class='content-wrapper' v-for='(obj, key) in services1' :key='key'>
+                <div class='service'>
+                                        <div class='img-wrapper'>
+                                            <img :src='obj.serviceIcn' :alt='obj.alt' data-aos='fade-left' />
                   </div>
 
-                                        <div class="copy-wrapper">
-                                            <h2 data-aos="fade-right">{{ obj.serviceHdr }}</h2>
-                                            <p data-aos="fade-left">{{ obj.serviceExcerpt }}</p>
+                                        <div class='copy-wrapper'>
+                                            <h2 data-aos='fade-right'>{{ obj.serviceHdr }}</h2>
+                                            <p data-aos='fade-left'>{{ obj.serviceExcerpt }}</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="device-img">
-                                <div class="img-wrapper">
+                            <div class='device-img'>
+                                <div class='img-wrapper'>
                                     <img
-                                        src="./../assets/imgs/home/iphone-x.png"
-                                        alt="image of iPhone"
-                                        data-aos="zoom-in"
+                                        src='./../assets/imgs/home/iphone-x.png'
+                                        alt='image of iPhone'
+                                        data-aos='zoom-in'
                                     />
                                 </div>
                             </div>
 
-                            <div class="services__group services__group--2">
-                                <div class="content-wrapper" v-for="(obj, two) in services2" :key="two">
-                <div class="service">
-                                    <div class="img-wrapper">
-                                        <img :src="obj.serviceIcn" :alt="obj.alt" data-aos="fade-right" />
+                            <div class='services__group services__group--2'>
+                                <div class='content-wrapper' v-for='(obj, two) in services2' :key='two'>
+                <div class='service'>
+                                    <div class='img-wrapper'>
+                                        <img :src='obj.serviceIcn' :alt='obj.alt' data-aos='fade-right' />
                   </div>
 
-                                    <div class="copy-wrapper">
-                                        <h2 data-aos="fade-left">{{ obj.serviceHdr }}</h2>
-                                        <p data-aos="fade-right">{{ obj.serviceExcerpt }}</p>
+                                    <div class='copy-wrapper'>
+                                        <h2 data-aos='fade-left'>{{ obj.serviceHdr }}</h2>
+                                        <p data-aos='fade-right'>{{ obj.serviceExcerpt }}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="section-img-desktop">
-                        <div class="img-wrapper">
+                    <div class='section-img-desktop'>
+                        <div class='img-wrapper'>
                             <img
-                                src="./../assets/imgs/home/iphone-x.png"
-                                alt="image of iPhone"
-                                data-aos="fade-right"
+                                src='./../assets/imgs/home/iphone-x.png'
+                                alt='image of iPhone'
+                                data-aos='fade-right'
                             />
                         </div>
                     </div>
--- src/components/what-we-do/what-we-do.component.jsx
+++ src/components/what-we-do/what-we-do.component.jsx
@@ -5,24 +5,24 @@ import './what-we-do.styles.scss'
 
 const WhatWeDo = () => {
     return (
-        <section id="what-we-do" className="what-we-do">
+        <section id='what-we-do' className='what-we-do'>
             <div className='container'>
-                <div className="content-wrapper">
-                    <h1 data-aos="fade-left">
+                <div className='content-wrapper'>
+                    <h1 data-aos='fade-left'>
                         What we
           <span>do.</span>
                     </h1>
 
-                    <div className="skills">
-                        <div className="skill skill-01">
-                            <div className="img-wrapper">
+                    <div className='skills'>
+                        <div className='skill skill-01'>
+                            <div className='img-wrapper'>
                                 <img
-                                    src={require("../../assets/imgs/home/img-01.jpg")}
-                                    alt="Chalk board"
-                                    data-aos="fade-right"
+                                    src={require('../../assets/imgs/home/img-01.jpg')}
+                                    alt='Chalk board'
+                                    data-aos='fade-right'
                                 />
                             </div>
-                            <div className="copy-wrapper" data-aos="fade-up">
+                            <div className='copy-wrapper' data-aos='fade-up'>
                                 <h2>
                                     From concept to
                                     completion
@@ -31,36 +31,36 @@ const WhatWeDo = () => {
                             </div>
                         </div>
 
-                        <div className="skill skill-02">
-                            <div className="img-wrapper">
-                                <img src={require("../../assets/imgs/home/img-02.jpg")}
-                                    alt="Coding website"
-                                    data-aos="fade-left" />
+                        <div className='skill skill-02'>
+                            <div className='img-wrapper'>
+                                <img src={require('../../assets/imgs/home/img-02.jpg')}
+                                    alt='Coding website'
+                                    data-aos='fade-left' />
                             </div>
-                            <div className="copy-wrapper" data-aos="fade-up">
+                            <div className='copy-wrapper' data-aos='fade-up'>
                                 <h2>Building for your brand</h2>
                                 <p>Working with you to enhance your digital presence, ensuring our creative solutions are all in line with your business goals.</p>
                             </div>
                         </div>
 
-                        <div className="skill skill-03-desktop">
-                            <div className="img-wrapper">
+                        <div className='skill skill-03-desktop'>
+                            <div className='img-wrapper'>
                                 <img
-                                    src={require("../../assets/imgs/home/img-05-3.jpg")}
-                                    alt="collaboration"
-                                    data-aos="fade-up"
+                                    src={require('../../assets/imgs/home/img-05-3.jpg')}
+                                    alt='collaboration'
+                                    data-aos='fade-up'
                                 />
                             </div>
-                            <div className="copy-wrapper">
-                                <h2 data-aos="fade-up">Bringing your vision to life</h2>
+                            <div className='copy-wrapper'>
+                                <h2 data-aos='fade-up'>Bringing your vision to life</h2>
                                 <p
-                                    className="skill-03"
-                                    data-aos="fade-up"
+                                    className='skill-03'
+                                    data-aos='fade-up'
                                 >You only have one chance to make a first impression. Let’s make it one that has your customers coming back for more.</p>
 
-                                <div className="azwun-btn azwun-btn--accent-border">
+                                <div className='azwun-btn azwun-btn--accent-border'>
                                     <Link to='/work'
-                                        className="azwun-btn__txt azwun-btn__txt--dark"
+                                        className='azwun-btn__txt azwun-btn__txt--dark'
                                     >See Our Work
                                     </Link>
                                 </div>
@@ -70,23 +70,23 @@ const WhatWeDo = () => {
                 </div>
             </div>
 
-            <div className="skill skill-03-mobile">
-                <div className="img-wrapper">
-                    <img src={require("../../assets/imgs/home/img-05-3.jpg")}
-                        alt="collaboration"
-                        data-aos="zoom-in" />
+            <div className='skill skill-03-mobile'>
+                <div className='img-wrapper'>
+                    <img src={require('../../assets/imgs/home/img-05-3.jpg')}
+                        alt='collaboration'
+                        data-aos='zoom-in' />
                 </div>
 
-                <div className="copy-wrapper">
-                    <h2 data-aos="fade-up">Bringing your vision to life</h2>
+                <div className='copy-wrapper'>
+                    <h2 data-aos='fade-up'>Bringing your vision to life</h2>
 
-                    <p data-aos="fade-up">
+                    <p data-aos='fade-up'>
                         You only have one chance to make a first impression. Let’s make it one that has your customers coming back for more.
                     </p>
 
-                    <div className="azwun-btn azwun-btn--accent-border" data-aos="zoom-in">
+                    <div className='azwun-btn azwun-btn--accent-border' data-aos='zoom-in'>
                         <Link to='work'
-                            className="azwun-btn__txt azwun-btn__txt--dark"
+                            className='azwun-btn__txt azwun-btn__txt--dark'
                         >See Our Work
                         </Link>
                     </div>
