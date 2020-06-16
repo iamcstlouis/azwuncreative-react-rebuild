babe24692309533cd8d2af2db3e9eb2be3c71e54
Chantal St Louis <misscstlouis@gmail.com>
2020-06-16 15:09:02 +0100

    Sets state for mobile menu and adds onClick functionality to open mobile menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,52 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 import './header.styles.scss'
 
-const Header = () => {
 
-    const toggleMobileMenu = () => {
-        var menuToggler = document.querySelector('.menu-toggler');
-        var hamburger = document.querySelector('.hamburger-btn');
-        var barTop = document.querySelector('.bar__top');
-        var barMid = document.querySelector('.bar__mid');
-        var barBtm = document.querySelector('.bar__btm');
-        var mobileMenu = document.querySelector('.mobile-menu');
+export default class Header extends Component {
 
-        var menuItems = [menuToggler, hamburger, barTop, barMid, barBtm, mobileMenu];
+    state = {
+        isOpen: false
+    }
 
-        menuItems.forEach(function (el) {
-            el.classList.toggle('open')
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
         })
     }
 
-    return (
-        <div id='nav'>
-            <div className='container'>
-                <div className='logo'>
-                    <div className='logo-wrapper'>
-                        <Link to='/'>
-                            <img
-                                src={require('../../assets/imgs/global/azwun-logo-mark2.svg')}
-                                className='logo'
-                                alt='Azwun Logo'
-                            />
-                        </Link>
+    render() {
+        return (
+            <div id='nav'>
+                <div className='container'>
+                    <div className='logo'>
+                        <div className='logo-wrapper'>
+                            <Link to='/'>
+                                <img
+                                    src={require('../../assets/imgs/global/azwun-logo-mark2.svg')}
+                                    className='logo'
+                                    alt='Azwun Logo'
+                                />
+                            </Link>
+                        </div>
                     </div>
-                </div>
 
-                <div className='menu-toggler' onClick={() => toggleMobileMenu()}>
-                    <div className='hamburger-btn'>
-                        <div className='bar bar__top'></div>
-                        <div className='bar bar__mid'></div>
-                        <div className='bar bar__btm'></div>
+                    <div className={`menu-toggler`} onClick={this.handleClick}>
+                        <div className='hamburger-btn'>
+                            <div className={`bar bar__top ${this.state.isOpen ? 'open' : undefined}`}></div>
+                            <div className={`bar bar__mid ${this.state.isOpen ? 'open' : undefined}`}></div>
+                            <div className={`bar bar__btm ${this.state.isOpen ? 'open' : undefined}`}></div>
+                        </div>
                     </div>
-                </div>
 
-                <div id='mobile-menu' className='mobile-menu'>
-                    <div className='mobile-menu-links'>
-                        <div className='container'>
+                    <div id='mobile-menu' className={`mobile-menu ${this.state.isOpen ? 'open' : undefined}`}>
+                        <div className='mobile-menu-links'>
+                            <div className='container'>
+                                <ul>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='/'>
+                                            Home
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='/work'>
+                                            Work
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='/about'>
+                                            About
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='contact'>
+                                            Contact
+                                        </NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='desktop-menu'>
+                        <div className='desktop-links'>
                             <ul>
                                 <li>
                                     <NavLink
@@ -80,44 +113,7 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
-
-                <div className='desktop-menu'>
-                    <div className='desktop-links'>
-                        <ul>
-                            <li>
-                                <NavLink
-                                    exact
-                                    to='/'>
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    exact
-                                    to='/work'>
-                                    Work
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    exact
-                                    to='/about'>
-                                    About
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    exact
-                                    to='contact'>
-                                    Contact
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
-
-export default Header;
