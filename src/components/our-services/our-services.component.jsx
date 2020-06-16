124f953326330ab435df4b2f5e463e2ba797edf8
Chantal St Louis <misscstlouis@gmail.com>
2020-06-16 16:56:04 +0100

    Finalises OurServices component importing data from both groups

--- src/components/our-services/our-services.component.jsx
+++ src/components/our-services/our-services.component.jsx
@@ -1,143 +1,67 @@
-import React, { Component } from 'react';
-import OurServicesData from './our-services.data';
+import React from 'react';
+import ServicesGroup1Data from './services-group1-data';
+import ServicesGroup2Data from './services-group2-data';
 
 import iphoneX from '../../assets/imgs/home/iphone-x.png';
 
 import './our-services.styles.scss';
 
-class OurServices extends Component {
-    // constructor(props) {
-    //     super(props);
 
-    //     this.state = {
-    //         services: []
-    //     }
-    // }
+const OurServices = () => {
+    return (
+        <section id='our-services' className='our-services'>
+            <div className='container'>
+                <div className='content-wrapper'>
+                    <h1 data-aos='fade-right'>
+                        Our <span>services.</span>
+                    </h1>
 
-    render() {
-        // const { services } = this.state;
-        return (
-            <section id='our-services' className='our-services'>
-                <div className='container'>
-                    <div className='content-wrapper'>
-                        <h1 data-aos='fade-right'>
-                            Our <span>services.</span>
-                        </h1>
-
-                        <div className='flex-container'>
-                            <div className='services'>
-                                <div className='services__group services__group--1'>
-                                    <OurServicesData />
-                                </div>
-
-                                <div className='device-img'>
-                                    <div className='img-wrapper'>
-                                        <img
-                                            src={iphoneX}
-                                            alt='iPhone'
-                                            data-aos='zoom-in'
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className='services__group services__group--2'>
-                                    <div className='content-wrapper' v-for='(obj, two) in services2'>
-                                        <div className='service'>
-                                            <div className='img-wrapper'>
-                                                {/* <img data-aos='fade-right' /> */}
-                                            </div>
-
-                                            <div className='copy-wrapper'>
-                                                <h2 data-aos='fade-left'>{}</h2>
-                                                <p data-aos='fade-right'>{}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                    <div className='flex-container'>
+                        <div className='services'>
+                            <div className='services__group services__group--1'>
+                                <ServicesGroup1Data />
                             </div>
 
-                            <div className='section-img-desktop'>
+                            <div className='device-img'>
                                 <div className='img-wrapper'>
                                     <img
                                         src={iphoneX}
                                         alt='iPhone'
-                                        data-aos='fade-right'
+                                        data-aos='zoom-in'
                                     />
                                 </div>
+                            </div>
+
+                            <div className='services__group services__group--2'>
+                                {/* <div className='content-wrapper' v-for='(obj, two) in services2'>
+                                    <div className='service'>
+                                        <div className='img-wrapper'>
+                                        </div>
+
+                                        <div className='copy-wrapper'>
+                                            <h2 data-aos='fade-left'></h2>
+                                            <p data-aos='fade-right'></p>
+                                        </div>
+                                    </div>
+                                </div> */}
+                                <ServicesGroup2Data />
+                            </div>
+                        </div>
+
+                        <div className='section-img-desktop'>
+                            <div className='img-wrapper'>
+                                <img
+                                    src={iphoneX}
+                                    alt='iPhone'
+                                    data-aos='fade-right'
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
 }
 
 export default OurServices;
-
-// import React from 'react';
-
-// import './our-services.styles.scss';
-// import OurServicesData from './our-services.data'
-
-
-// const OurServices = () => {
-//     return (
-//         <section id='our-services' className='our-services'>
-//                  <div className='container'>
-//                      <div className='content-wrapper'>
-//                          <h1 data-aos='fade-right'>
-//                              Our <span>services.</span>
-//                          </h1>
-
-//                          <div className='flex-container'>
-//                              <div className='services'>
-//                                  <div className='services__group services__group--1'>
-//                                      <div className='content-wrapper'>
-//                                          {/* <OurServicesData /> */}
-//                                      </div>
-//                                  </div>
-
-//                                  <div className='device-img'>
-//                                      <div className='img-wrapper'>
-//                                          <img
-//                                             src={require('../../assets/imgs/home/iphone-x.png')}
-//                                             alt='iPhone'
-//                                             data-aos='zoom-in'
-//                                         />
-//                                     </div>
-//                                 </div>
-
-//                                 <div className='services__group services__group--2'>
-//                                     <div className='content-wrapper' v-for='(obj, two) in services2'>
-//                                         <div className='service'>
-//                                             <div className='img-wrapper'>
-//                                                 {/* <img data-aos='fade-right' /> */}
-//                                             </div>
-
-//                                             <div className='copy-wrapper'>
-//                                                 <h2 data-aos='fade-left'>{}</h2>
-//                                                 <p data-aos='fade-right'>{}</p>
-//                                             </div>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-
-//                             <div className='section-img-desktop'>
-//                                 <div className='img-wrapper'>
-//                                     <img
-//                                         src={require('../../assets/imgs/home/iphone-x.png')}
-//                                         alt='iPhone'
-//                                         data-aos='fade-right'
-//                                     />
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </section>
-//     );
-// }
-
-// export default OurServices;
---
+++ src/components/our-services/services-group2-data.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import Group2Service from './group2-service.component';
+
+const ServicesGroup2Data = () => {
+    // const [services, setServices] = useState([
+    const [services] = useState([
+        {
+            serviceIcn: require('../../assets/imgs/home/icn_content-writing_2.png'),
+            alt: 'Azwun Creative Content Writing Icon',
+            serviceHdr: 'Content Writing',
+            serviceExcerpt:
+                'Finding the right words to ensure your customers are crystal clear on the service you provide.',
+            id: 4
+        },
+        {
+            serviceIcn: require('../../assets/imgs/home/icn_maintenance.png'),
+            alt: 'Azwun Creative Maintenance Icon',
+            serviceHdr: 'Maintenance',
+            serviceExcerpt:
+                'Building your website is just the start. We’ll ensure your site remains compatible across all browsers and devices.',
+            id: 5
+        }
+    ])
+
+    return (
+        <div>
+            {services.map(service => (
+                <Group2Service serviceIcn={service.serviceIcn} alt={service.alt} serviceHdr={service.serviceHdr} serviceExcerpt={service.serviceExcerpt} key={service.id} />
+            ))}
+        </div>
+    );
+}
+
+export default ServicesGroup2Data;
